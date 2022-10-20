14ba41cb686aad95d88d923b7f6b3598cb3931d1
Chitraksh Kumar <chitraksh24@gmail.com>
2022-10-20 10:46:46 -0700

    Update Assignment4.py

--- Assignment4.py
+++ Assignment4.py
@@ -16,14 +16,27 @@ def main():
 
     r2_scores = []
 
+    minimum_r2 = -1000000
+    best_coeff = np.array([])
+    best_lr = 0
+    best_rp = 0
+
     for lrates in lr:
         for rp in lambdas:
             model = GradientDescentWRRCK(lrates,50,rp)
             model.fit(X_train,Y_train)
             Y_pred = model.predict(X_test)
             r2_scores.append(r2_score(Y_pred,Y_test))
+            if minimum_r2 < r2_score(Y_pred,Y_test):
+                best_coff = model.B
+                best_lr = lrates
+                best_rp = rp
+                minimum_r2 = r2_score(Y_pred,Y_test)
 
-    print(r2_scores)
+    print("Best Coefficiants:")
+    print(best_coff)
+    print("Best Learning rate:",best_lr)
+    print("Best regularization parameter:",best_rp)
 
 if __name__ == "__main__":
     main()
